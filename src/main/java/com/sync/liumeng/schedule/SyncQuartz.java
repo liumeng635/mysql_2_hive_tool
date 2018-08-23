d843636530834e271fc5458337a2cb320f82a59b
liumeng635 <635891879@qq.com>
2018-08-23 09:49:22 +0800

    包结构调整

--- src/main/java/com/sync/liumeng/schedule/SyncQuartz.java
+++ src/main/java/com/sync/liumeng/schedule/SyncQuartz.java
@@ -1,10 +1,9 @@
-package com.sync.liumeng.config.Schedule;
+package com.sync.liumeng.schedule;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
